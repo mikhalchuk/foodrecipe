83070ef0b64b543cbb861fa5677c3c097c8f5435
mikhalchuk <sergii.mykhalchuk@gmail.com>
2015-01-04 19:13:24 +0200

    add search client and logic to crawler command

--- config/container.php
+++ config/container.php
@@ -3,14 +3,24 @@
 $container = new Pimple\Container();
 
 $container['logger'] = function ($container) {
-      return new Monolog\Logger(
-          'logger',
-          [new Monolog\Handler\StreamHandler(realpath(__DIR__) . '/log.txt')]
-      );
+    return new Monolog\Logger(
+        'logger',
+        [new Monolog\Handler\StreamHandler(realpath(__DIR__) . '/log.txt')]
+    );
+};
+
+$container['searchClient'] = function ($container) {
+    $params['logging']   = true;
+    $params['logObject'] = $container['logger'];
+    $client = new Elasticsearch\Client($params);
+    /*$client->indices()->create([
+        'index' => 'recipes'
+    ]);*/
+    return $client;
 };
 
 $container['httpClient'] = function ($container) {
-    return new Guzzle\Http\Client();
+    return new Guzzle\Http\Client('http://gotovim-doma.ru/');
 };
 
 return $container;
--- src/Commands/CrawlerCommand.php
+++ src/Commands/CrawlerCommand.php
@@ -4,6 +4,7 @@ namespace FoodRecipe\Command;
 use FoodRecipe\System\Framework\ConsoleCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DomCrawler\Crawler;
 
 class CrawlerCommand extends ConsoleCommand
 {
@@ -18,5 +19,79 @@ class CrawlerCommand extends ConsoleCommand
     {
         /** @var \Guzzle\Http\Client $httpClient */
         $httpClient = self::$container['httpClient'];
+
+        /** @var \Elasticsearch\Client $searchClient */
+        $searchClient = self::$container['searchClient'];
+
+        $httpHeaders = [
+            'Accept' => "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            'Accept-Encoding' => 'gzip, deflate, sdch',
+            'Cache-Control' => 'max-age=0',
+            'Connection' => 'keep-alive',
+            'Accept-Language' => 'en-US',
+            'User-Agent' => 'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0',
+        ];
+        $httpOptions = [
+            'timeout' => 5,
+        ];
+
+        $uri = '/view.php?g=';
+        $i = 0;
+        foreach (range(1, 45) as $number) {
+            $recipesHtml = $httpClient->get($uri . $number, $httpHeaders, $httpOptions)->send()->getBody(true);
+
+            $list = [];
+            try {
+                $list = (new Crawler($recipesHtml))->filterXPath(
+                    '//*[@id="content"]/span[1]/table/tr[*]/td[position() = 2 or position() = 4]/a'
+                )->each(function (Crawler $node, $i) {
+                    return $node->attr('href');
+                });
+
+                if (!empty($list)) {
+                    foreach ($list as $item) {
+                        $recipeHtml = $httpClient->get($item, $httpHeaders, $httpOptions)->send()->getBody(true);
+                        try {
+                            $recipeCrawler = new Crawler($recipeHtml);
+
+                            $rawIngredients = $recipeCrawler->filterXPath(
+                                '//*[@id="content"]/div[2]/ul[1]/li[(contains(@class, "ingredient"))]'
+                            )->each(function (Crawler $node, $i) {
+                                return $node->text();
+                            });
+
+                            $ingredients = [];
+                            foreach ($rawIngredients as $ingredient) {
+                                $matches = [];
+                                if (preg_match('/(.*) - (.*)/', $ingredient, $matches) === 1) {
+                                    $ingredients[] = $matches[1];
+                                } else {
+                                    $ingredients[] = $ingredient;
+                                }
+                            }
+                            unset($ingredient);
+
+                            $recipe = [
+                                'title' => $recipeCrawler->filterXPath('//*[@id="content"]/div[2]/h1/a')->text(),
+                                'ingredients' => $ingredients,
+                                'raw' => $rawIngredients,
+                            ];
+
+                            $data = $searchClient->index([
+                                'index' => 'recipes',
+                                'type' => 'recipe',
+                                'id' => ++$i,
+                                'body' => $recipe,
+                            ]);
+                        } catch (\InvalidArgumentException $e) {
+                            continue;
+                        }
+                    }
+                }
+
+            } catch (\InvalidArgumentException $e) {
+                continue;
+            }
+        }
     }
 }
