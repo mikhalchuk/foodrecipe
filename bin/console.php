859a1f500729cc84b6e97aff4899a8ff0c302cac
mikhalchuk <sergii.mykhalchuk@gmail.com>
2015-01-04 19:12:16 +0200

    use monolog as errors handler

--- bin/console.php
+++ bin/console.php
@@ -10,6 +10,8 @@ use FoodRecipe\Command\CrawlerCommand;
 $dc = require_once dirname(__FILE__) . '/../config/container.php';
 ConsoleCommand::setContainer($dc);
 
+Monolog\ErrorHandler::register($dc['logger']);
+
 $application = new Application();
 $application->add(new CrawlerCommand());
 $application->run();
